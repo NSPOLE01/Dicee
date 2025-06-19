d77760d8a7b4e1193bb4907dcde1627d7283d9f5
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-06-19 17:11:39 -0400

    connecting to code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.image = UIImage(named: "DiceSix")
+        diceImageViewTwo.image = UIImage(named: "DiceTwo")
+        
     }
 
 

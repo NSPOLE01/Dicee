5af0ad736cdd70ef84686515a8cb12188af869e8
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-06-19 20:03:37 -0400

    dices change based on user input

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,14 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
+    let diceImages: [UIImage] = [UIImage(named: "DiceOne")!, UIImage(named: "DiceTwo")!, UIImage(named: "DiceThree")!, UIImage(named: "DiceFour")!, UIImage(named: "DiceFive")!, UIImage(named: "DiceSix")! ]
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageViewOne.image = UIImage(named: "DiceSix")
-        diceImageViewTwo.image = UIImage(named: "DiceTwo")
-        
+
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        leftDiceNumber = Int.random(in: 0...5)
+        rightDiceNumber = Int.random(in: 0...5)
+        diceImageViewOne.image = diceImages[leftDiceNumber]
+        diceImageViewTwo.image = diceImages[rightDiceNumber]
+    }
+    
 }
 
